6e65c142fc0332dd795f234e6f577d344f7fc50b
Siddhesh Bhelke <139620000+Siddhesh5004@users.noreply.github.com>
2024-05-28 10:22:03 +0530

    new topic

--- 01-Basics/06_Strings.js
+++ 01-Basics/06_Strings.js
@@ -6,7 +6,22 @@ console.log('hello my name is ${name and my repo count is $ {repoCount}');
 
 //how to declare strings
 
-const gameName=new String('siddhesh')
-console.log(gameName);
-console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
+const gameName=new String('siddhesh-sh')
+//console.log(gameName);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(2));
+console.log(gameName.indexOf('d'));
+
+const newString=gameName.substring(0,4)
+console.log(newString);
+
+const anotherString =gameName.slice(-8,4)
+console.log(anotherString);
+
+//Trim and replace
+
+const newStringOne = "   hitesh  "
+console.log(newStringOne.trim());
+
+
+
---
+++ 01-Basics/07_Nums & Math.js
@@ -0,0 +1,10 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+//converting number to string and string properties
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+
