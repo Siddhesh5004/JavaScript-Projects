ecba6afd3844e200065a70d8370e08fc475c2b9c
Siddhesh Bhelke <139620000+Siddhesh5004@users.noreply.github.com>
2024-05-30 09:43:56 +0530

    added arrays

---
+++ 02-Arrays/01_arrays.js
@@ -0,0 +1,22 @@
+//array
+
+const myArr =[0,5,2,7,9,8]
+const myHeros =["shaktiman","virat"]
+const myArr2 =new Array(1,2,3,4,5)
+console.log(myArr[4]);
+
+// Array Methods
+// myArr.push(6)
+// myArr.push(10)
+// myArr.pop()
+
+
+myArr.unshift(9)
+myArr.shift()
+console.log(myArr);
+
+
+
+
+
+
