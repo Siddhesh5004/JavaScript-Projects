93b63f7485e1f57195b8efcc5a591dfd5b804816
Siddhesh Bhelke <139620000+Siddhesh5004@users.noreply.github.com>
2024-05-31 11:45:41 +0530

    updated API

--- 02-Arrays/04_Objects.js
+++ 02-Arrays/04_Objects.js
@@ -57,3 +57,16 @@ const course ={
 console.log(course.courseInstructor);
 
 
+[
+    {},
+    {},
+    {}
+]
+
+// API data of from random user api 
+[
+{"results":[{"gender":"female","name":{"title":"Ms","first":"Vanessa","last":"Alfsen"},"location":{"street":{"number":8154,"name":"Johan Castbergs vei"},"city":"Hellesylt","state":"Akershus","country":"Norway","postcode":"6063","coordinates":{"latitude":"46.5304","longitude":"102.0390"},"timezone":{"offset":"-1:00","description":"Azores, Cape Verde Islands"}},"email":"vanessa.alfsen@example.com","login":{"uuid":"f0b6d640-3b56-4cd4-b67c-c31b09b7a3bb","username":"smallrabbit277","password":"jorge","salt":"TtyXZq7y","md5":"8fe451a4fca68e36468f7c38bacc8f44","sha1":"4eaa25457899cabf6c9b2a7c6fd0672611e5c33a","sha256":"f60a2bb2ea6e0437bd29d6e7d0a468c0e8a7bf3d5e2fa83c670b300c3844ce3b"},"dob":{"date":"1949-09-18T14:07:53.107Z","age":74},"registered":{"date":"2016-06-27T05:37:07.064Z","age":7},"phone":"67462771","cell":"45796295","id":{"name":"FN","value":"18094907043"},"picture":{"large":"https://randomuser.me/api/portraits/women/0.jpg","medium":"https://randomuser.me/api/portraits/med/women/0.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/0.jpg"},"nat":"NO"}],"info":{"seed":"4ab0c22fc1531288","results":1,"page":1,"version":"1.4"}}
+]
+
+
+
