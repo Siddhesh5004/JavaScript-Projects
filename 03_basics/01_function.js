c2d90eeb685972ff727b7434b8196de05f1aec70
Siddhesh Bhelke <139620000+Siddhesh5004@users.noreply.github.com>
2024-05-31 13:27:25 +0530

    updated

---
+++ 03_basics/01_function.js
@@ -0,0 +1,38 @@
+//traditional way 
+//console.log("s");
+//console.log("i");
+//console.log("d");
+
+//Function
+
+function sayMyname (){
+    console.log("s");
+console.log("i");
+console.log("d");
+}
+
+//sayMyname()
+
+// function addtwonos (no1,no2){  //parameters
+
+//     console.log (no1+no2);
+// }
+function addtwonos (no1,no2){  //parameters
+
+    // let result = no1+no2
+    // return result
+   return no1+no2
+}
+
+const result =addtwonos(3,5) // function called
+
+// console.log( "Result : ",result);
+
+function loginuser (username){
+    return `${username} just logged in `
+}
+
+console.log((loginuser ('siddhesh'))); 
+
+
+
