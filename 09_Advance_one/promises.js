ffd20ed253382ee2c746b7ba8ddf9f75dd574f4c
Siddhesh Bhelke <139620000+Siddhesh5004@users.noreply.github.com>
2024-07-04 14:54:42 +0530

    done promises

--- 09_Advance_one/promises.js
+++ 09_Advance_one/promises.js
@@ -92,4 +92,18 @@ consumePromiseFive()
 //getAllUsers()
 
 
+fetch('https:api.github.com/users/hiteshchoudhary')
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data);
+})
+.catch(function(error){
+    console.log(error);
+})
+
+
+
+
 
